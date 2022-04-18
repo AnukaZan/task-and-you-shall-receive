b889c8cecb6d448b4cacd1e6917624a0216c706e
Anuka <anukazan@gmail.com>
2022-04-17 17:27:48 -0700

    refractoring task add function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event){
+var taskFormHandler = function(event){
     event.preventDefault(); //cancels event if its cancellable
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj){
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -16,11 +25,11 @@ var createTaskHandler = function(event){
     //give it a class name
     taskInfoEl.className = "task-info";
     //use HTML to bold task-name and add task type below it in div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl); // add the taskInfoEl div to the <li>
 
     tasksToDoEl.appendChild(listItemEl); //add entire list item to tasksToDoEl
-};
+}
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
